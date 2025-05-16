c7452a7ed2979d04049f52910c1e7e6d3f6ed725
Dmitry Maximyuk <d_maksimyk@vk.com>
2025-05-16 16:26:32 +0500

    Init base struct

--- src/app.tsx
+++ src/app.tsx
@@ -2,22 +2,11 @@ import "./app.css";
 
 import { useState } from "preact/hooks";
 
-import viteLogo from "/vite.svg";
-import preactLogo from "./assets/preact.svg";
-
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
